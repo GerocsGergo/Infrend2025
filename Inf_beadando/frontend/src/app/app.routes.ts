3d6f88fe7fdac3d77c8289a0bc19e24f898774ef
GerocsGergo <90260072+GerocsGergo@users.noreply.github.com>
2025-04-29 13:33:28 +0200

    Delete Inf_beadando directory

--- Inf_beadando/frontend/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { WelcomeComponent } from './components/welcome/welcome.component';
-
-export const routes: Routes = [
-    { path: '', component: WelcomeComponent }
-
-];
